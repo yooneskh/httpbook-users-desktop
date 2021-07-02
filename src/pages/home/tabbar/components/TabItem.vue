59dd6270d6b8bbcf838f58875bb1657ea59c34b7
Yoones Khoshghadam <yooneskh@gmail.com>
2021-07-02 19:11:37 +0430

    fix -tabs- tab item styling

--- src/pages/home/tabbar/components/TabItem.vue
+++ src/pages/home/tabbar/components/TabItem.vue
@@ -12,13 +12,13 @@ const props = defineProps({
 
 <template>
   <div class="tab-item" :class="{ 'active' : props.active }" style="height: 100%;">
-    <div class="wrapper px-3 d-flex flex-row align-center" style="height: 100%;" @click.self="$emit('click')">
+    <div class="wrapper px-2 d-flex flex-row align-center" style="height: 100%;" @click.self="$emit('click')">
 
       <span class="text-caption" @click="$emit('click')">
         ApiName
       </span>
 
-      <v-btn v-if="active" text icon class="ms-3" size="auto" style="padding: 1px" @click="$emit('close')">
+      <v-btn v-if="active" text icon class="ms-1" size="auto" style="padding: 1px" @click="$emit('close')">
         <v-icon size="12">mdi-close</v-icon>
       </v-btn>
 
@@ -27,6 +27,10 @@ const props = defineProps({
 </template>
 
 <style lang="scss" scoped>
+  .tab-item {
+    cursor: pointer;
+    user-select: none;
+  }
   .tab-item.active {
     background-color: rgba(white, 0.0325);
   }
